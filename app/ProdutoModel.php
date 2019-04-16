04e1095e9692d2dcb5956c6c90427c5ead1660ef
Cleiton080 <cleitonpessoa53@hotmail.com>
2019-04-15 21:55:00 -0300

    Nome da tabela alterada

--- app/ProdutoModel.php
+++ app/ProdutoModel.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProdutoModel extends Model
 {
-    protected $table = 'product';
+    protected $table = 'produto';
 
     protected $guard = ['id'];
 
